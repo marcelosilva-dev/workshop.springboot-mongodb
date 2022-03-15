0fe81077cf1b3af70f92f3ce4359e1751ae92f0e
marcelosilva-dev <marceloeduardo244@gmail.com>
2022-03-15 12:42:37 -0300

    Consulta simples com query method

--- src/main/java/com/pbidevs/workshopmongo/resourses/PostResourse.java
+++ src/main/java/com/pbidevs/workshopmongo/resourses/PostResourse.java
@@ -1,24 +1,17 @@
 package com.pbidevs.workshopmongo.resourses;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pbidevs.workshopmongo.domain.Post;
-import com.pbidevs.workshopmongo.domain.User;
-import com.pbidevs.workshopmongo.dto.UserDTO;
+import com.pbidevs.workshopmongo.resourses.util.URL;
 import com.pbidevs.workshopmongo.services.PostService;
 
 @RestController
@@ -36,12 +29,11 @@ public class PostResourse {
 		return ResponseEntity.ok().body(post);
 	}
 	
-	
-	@DeleteMapping(value="/{id}")
-	public ResponseEntity<Void> deleteBiId(@PathVariable String id) {
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+	 List<Post> list = postService.findByTitle(text);
 		
-		postService.delete(id);
-		
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ src/main/java/com/pbidevs/workshopmongo/resourses/util/URL.java
@@ -0,0 +1,14 @@
+package com.pbidevs.workshopmongo.resourses.util;
+
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (Exception e) {
+			return "";
+		}
+	}
+}
