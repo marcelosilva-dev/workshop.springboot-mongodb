b078998c10c20da53fc3c9b415010a84e47eacd3
marcelosilva-dev <marceloeduardo244@gmail.com>
2022-03-15 11:22:34 -0300

    Incluido comentarios nos posts

--- src/main/java/com/pbidevs/workshopmongo/domain/Post.java
+++ src/main/java/com/pbidevs/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.pbidevs.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.pbidevs.workshopmongo.dto.AuthorDTO;
+import com.pbidevs.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -86,7 +99,5 @@ public class Post implements Serializable {
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}
-	
-	
+	}	
 }
